ace16c1ec3b06ccc900c9f5f71eef285e0a06b3c
Antonio <antoniosansotta@gmail.com>
2022-03-17 17:37:19 -0400

    First Commit, version 1.0 upload

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// Dom Selectors
+let mobileNavIcon = document.querySelector(".mobileNavIcon")
+let menuSelection = document.querySelector(".menuSelectionNav")
+
+// Dom Functions
+
+// Event Listeners
+mobileNavIcon.addEventListener("click", mobileNavToggle = () => {
+    document.querySelector(".nav-mobileMenu").classList.toggle("hidden")
+ })
+
+const toggleMenuItems = (e) => {
+    console.log("clicked")
+    let pizza = document.querySelector(".pizzas")
+    let sides =  document.querySelector(".sides")
+    let drinks =  document.querySelector(".drinks")
+    let desserts =  document.querySelector(".desserts")
+    
+    pizza.style.display = "none"
+    sides.style.display = "none"
+    drinks.style.display = "none"
+    desserts.style.display = "none"
+    
+    
+    console.log(e.target.id)
+     switch(e.target.id){
+         case 'menuPizza':
+            pizza.style.display = "flex"
+            break
+        case 'menuSides':
+            sides.style.display = "flex"
+            break
+        case 'menuDrinks':
+            drinks.style.display = "flex"
+            break
+        case 'menuDesserts':
+            desserts.style.display = "flex"
+            break
+     }
+}
+
+menuSelection.addEventListener("click", toggleMenuItems)
+
+ 
+
+ 
